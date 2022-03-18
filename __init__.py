b1fc7dbea80f4838e92f5e42fe191febee3a89ab
Nikolay <krukolnik@gmail.com>
2022-03-18 15:17:14 +0300

    Дописал класс Game, вынес его в отдельный файл и добавил реакции боту на победы

--- __init__.py
+++ __init__.py
@@ -2,89 +2,17 @@
 
 import config
 import telebot
-import random
+from Game import Game
 
 bot = telebot.TeleBot(config.telegram_token)
-
-class Game:
-    __id_all = 0                # статическое поле для id
-    __games_list = dir()        # словарь созданных игр
-    __attempts_max = 5            # максимальное кол-во попыток
-    __game_status_list = {      # словарь статусов игры
-        0: 'Игра не начата',
-        1: 'Игры идет',
-        2: 'Игра выиграна',
-        3: 'Игра проиграна'
-    }
-
-    def __init__(self):
-       self.__attempts_current = 0                       # кол-во сделанных попыток
-       self.__secret_value = 0                           # загаданное число
-       self.__last_attempt = 0                           # последняя попытка пользователя
-       self.__game_id = Game.__id_all                    # id игры
-       self.__game_status = 0                            # статус игры
-       Game.__id_all = Game.__id_all + 1
-       Game.__games_list[self.__game_id] = self          # сохраняем игру в словарь
-
-    #----------------------Свойства-----------------------
-    @property
-    def attempts_current(self):
-        return self.__attempts_current
-
-    @property
-    def game_id(self):
-        return self.__game_id
-
-    @property
-    def attempts_current(self):
-        return self.__attempts_current
-
-    @property
-    def game_status(self):
-        return Game.__game_status_list[self.__games]
-
-    @property
-    def get_possible_statuses_game(self):
-        # возвращаем строку всех возможных статусов игры
-        status_list = list()
-        for key in Game.__game_status_list:
-            status_list.append(str(key) + ' - ' + Game.__game_status_list[key] + ';')
-        return ' '.join(status_list)
-
-    # ---------------------Методы-----------------------
-    # начать игру
-    def start_game(self):
-        self.__secret_value = random.randint(0, 99)
-        self.__game_status = 1
-
-    # сделать попытку по угадыванию числа
-    def make_attempt(self, value):
-        self.__last_attempt = value                                 # обновляем значение крайней попытки
-
-        if value == self.__secret_value and self.__game_status == 1 and self.__attempts_current() <= Game.__attempts_max:
-            self.__game_status = 2                                  # фиксируем победу
-            return True
-        else:
-            return False
-
-    # # # обновление статуса игры
-    # def _update_game_status(self):
-    #     if self.__last_attempt == self.__secret_value :            # если число было угадано
-    #         self.__attempts_current = self.__attempts_current + 1  # обновляем кол-во попыток
-    #         self.__
-
-
-
-
-
-
-game = None
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет, друг! Я хочу сыграть с тобой в одну игру…')
     bot.send_message(message.chat.id, 'Начнем?')
+    config.game = Game()                    # создаем игру
+
 
 @bot.message_handler(commands=['rules'])
 def start(message):
@@ -96,9 +24,42 @@ def start(message):
 
 @bot.message_handler(commands=['start game'])
 def start(message):
+    if config.game is None:
+        config.game = Game()
+    config.game.start_game()
     bot.send_message(message.chat.id, 'А ты смельчак! Ну что ж, начнем игру!')
+    bot.send_message(message.chat.id, 'Я загадал число от 0 до 99. Жду от тебя ответа.')
+    bot.send_message(message.chat.id, 'Даю тебе ' + str(config.game.number_available_attempts) + ' попыток')
 
 
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if type(message.text) != int:
+        bot.send_message(message.chat.id, 'Наверно стоит попробовать ввести число...')
+        bot.send_message(message.chat.id, 'Так уж и быть попытку забирать не буду')
+        return
+
+    answer = int(message.text)
+
+    if config.game.make_attempt():
+        attempt_cur = config.game.attempts_current # получили кол-во доступных попыток
+        if attempt_cur == 1:
+            bot.send_message(message.chat.id, 'Ого! Так легко отделались... Везение? Или суровый расчет? '
+                                              'В любом случае поздравляю! Вы молодец!')
+        elif attempt_cur == 2:
+            bot.send_message(message.chat.id, 'Эх, зря подсказывал! Так бы еще поиграли. Поздравляю! Вы победили!')
+        elif attempt_cur == 3:
+            bot.send_message(message.chat.id, 'Неплохо сыграно! Поздравляю Вас с победой!')
+        elif attempt_cur == 4:
+            bot.send_message(message.chat.id, 'Еще чуть-чуть и я бы победил :( \nВы молодец, поздравляю вас!')
+        elif attempt_cur == 5:
+            bot.send_message(message.chat.id, 'На грани! Думал, что Вы уже не победите! Но Вы справились и собрались в последний момент!'
+                                              '\nПоздравляю Вас с победой!')
+
+
+    if int(message.text) > config.game.value_max or int(message.text) < config.game.value_min:
+        bot.send_message(message.chat.id, 'Хм, ответ интересный... Наверное, стоит попробовать вводить числа в заданном диапазоне =) \n'
+                                          'Может быть так получится угадать?')
 
 
 
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 telegram_token = os.environ['telegramToken']
 
+game = None
